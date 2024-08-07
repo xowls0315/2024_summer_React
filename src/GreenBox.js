ab4ec5024253a84b6268ca1d6237e4ce72a8577b
xowls0315 <155832601+xowls0315@users.noreply.github.com>
2024-08-07 14:45:18 +0900

    Add files via upload

---
+++ src/GreenBox.js
@@ -0,0 +1,8 @@
+const GreenBox = () => {
+
+    const GreenStyle = {width:"100px", height:"100px", backgroundColor:"green"};
+
+    return <div style={GreenStyle}></div>
+}
+
+export default GreenBox;
---
+++ src/YellowCricle.js
@@ -0,0 +1,7 @@
+const YellowCircle = () => {
+    const YellowStyle = { width: "100px", height: "100px", backgroundColor:"yellow", borderRadius: "50%", margin: "30px" };
+
+    return <div style={YellowStyle}></div>
+}
+
+export default YellowCircle;
